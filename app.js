292a13c29b42a44e61639b965ee1b6fe18acb49f
Yazid J. Alaoui <yazid.ja@gmail.com>
2021-03-11 01:22:54 +0100

    +W/L/D Counter
    Document formatting + counter functionality

--- app.js
+++ app.js
@@ -3,38 +3,68 @@ const paperBtn = document.getElementById("paperBtn");
 const scissorsBtn = document.getElementById("scissorsBtn");
 const output1 = document.getElementById("output1");
 const output2 = document.getElementById("output2");
+const counter = document.getElementById("counter");
+let wins = 0;
+let draws = 0;
+let losses = 0;
+
 let playerChoice;
 let gameArr = ["rock", "paper", "scissors"];
 let gameResult;
 
 function endGame() {
-    console.log(`${gameResult}: You chose ${playerChoice} and the game chose ${gameChoice}`)
-    output1.textContent = gameResult;
-    output2.textContent = `You chose ${playerChoice} and the game chose ${gameChoice}`
+  console.log(`${gameResult}: You chose ${playerChoice} and the game chose ${gameChoice}`);
+  output1.textContent = gameResult;
+  output2.textContent = `You chose ${playerChoice} and the game chose ${gameChoice}`;
+  if (gameResult == "DRAW") {
+    draws += 1;
+  } else if (gameResult == "YOU LOSE") {
+    losses += 1;
+  } else if (gameResult == "YOU WIN") {
+    wins += 1;
+  }
+  counter.textContent = `Wins: ${wins} / Losses: ${losses} / Draws: ${draws}`;
 }
 
 function rock() {
-    playerChoice = "rock";
-    gameChoice = gameArr[parseInt(Math.random()*3)];
-    if (gameChoice == "rock") {gameResult = "DRAW"} else if (gameChoice == "paper") {gameResult = "YOU LOSE"} else if (gameChoice == "scissors") {gameResult = "YOU WIN"}
-    endGame()
+  playerChoice = "rock";
+  gameChoice = gameArr[parseInt(Math.random() * 3)];
+  if (gameChoice == "rock") {
+    gameResult = "DRAW";
+  } else if (gameChoice == "paper") {
+    gameResult = "YOU LOSE";
+  } else if (gameChoice == "scissors") {
+    gameResult = "YOU WIN";
   }
+  endGame();
+}
 
-  function paper() {
-    playerChoice = "paper";
-    gameChoice = gameArr[parseInt(Math.random()*3)];
-    if (gameChoice == "paper") {gameResult = "DRAW"} else if (gameChoice == "scissors") {gameResult = "YOU LOSE"} else if (gameChoice == "rock") {gameResult = "YOU WIN"}
-    endGame()
+function paper() {
+  playerChoice = "paper";
+  gameChoice = gameArr[parseInt(Math.random() * 3)];
+  if (gameChoice == "paper") {
+    gameResult = "DRAW";
+  } else if (gameChoice == "scissors") {
+    gameResult = "YOU LOSE";
+  } else if (gameChoice == "rock") {
+    gameResult = "YOU WIN";
   }
+  endGame();
+}
 
-  function scissors() {
-    playerChoice = "scissors";
-    gameChoice = gameArr[parseInt(Math.random()*3)];
-    if (gameChoice == "scissors") {gameResult = "DRAW"} else if (gameChoice == "rock") {gameResult = "YOU LOSE"} else if (gameChoice == "paper") {gameResult = "YOU WIN"}
-    endGame()
+function scissors() {
+  playerChoice = "scissors";
+  gameChoice = gameArr[parseInt(Math.random() * 3)];
+  if (gameChoice == "scissors") {
+    gameResult = "DRAW";
+  } else if (gameChoice == "rock") {
+    gameResult = "YOU LOSE";
+  } else if (gameChoice == "paper") {
+    gameResult = "YOU WIN";
   }
+  endGame();
+}
 
 rockBtn.addEventListener("click", rock);
 paperBtn.addEventListener("click", paper);
 scissorsBtn.addEventListener("click", scissors);
-
