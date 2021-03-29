0d10627a025339eb153a20e844f7e11706588f78
Yazid J. Alaoui <yazid.ja@gmail.com>
2021-03-29 23:43:31 +0100

    Refactoring + Styling

--- app.js
+++ app.js
@@ -1,69 +1,42 @@
+// Elements
 const rockBtn = document.getElementById("rockBtn");
 const paperBtn = document.getElementById("paperBtn");
 const scissorsBtn = document.getElementById("scissorsBtn");
-const output1 = document.getElementById("output1");
-const output2 = document.getElementById("output2");
+const title = document.getElementById("title");
+const output = document.getElementById("output");
 const counter = document.getElementById("counter");
+// Variables
 let wins = 0;
 let draws = 0;
 let losses = 0;
-
 let playerChoice;
-let gameArr = ["ROCK", "PAPER", "SCISSORS"];
 let gameResult;
-
-function endGame() {
-  output1.textContent = gameResult;
-  output2.textContent = `You picked ${playerChoice} and your opponent picked ${gameChoice}`;
-  if (gameResult == "DRAW") {
-    draws += 1;
-  } else if (gameResult == "YOU LOSE") {
-    losses += 1;
-  } else if (gameResult == "YOU WIN") {
+const arr = ["rock", "paper", "scissors"];
+// Functions
+gameHandler = (playerChoice) => {
+  let gameChoice = Math.floor(Math.random() * 3);
+  let result = playerChoice - gameChoice;
+  if (result == 1 || result == -2) {
+    gameResult = "YOU WIN";
     wins += 1;
+  } else if (result === 0) {
+    gameResult = "DRAW";
+    draws += 1;
+  } else {
+    gameResult = "YOU LOSE";
+    losses += 1;
   }
+  title.textContent = gameResult;
+  output.innerHTML = `You picked <i class="far fa-hand-${arr[playerChoice]}"></i>  and your opponent picked <i class="far fa-hand-${arr[gameChoice]}"></i>`;
   counter.textContent = `Wins: ${wins} / Losses: ${losses} / Draws: ${draws}`;
-}
-
-function rock() {
-  playerChoice = "ROCK";
-  gameChoice = gameArr[parseInt(Math.random() * 3)];
-  if (gameChoice == "ROCK") {
-    gameResult = "DRAW";
-  } else if (gameChoice == "PAPER") {
-    gameResult = "YOU LOSE";
-  } else if (gameChoice == "SCISSORS") {
-    gameResult = "YOU WIN";
-  }
-  endGame();
-}
-
-function paper() {
-  playerChoice = "PAPER";
-  gameChoice = gameArr[parseInt(Math.random() * 3)];
-  if (gameChoice == "PAPER") {
-    gameResult = "DRAW";
-  } else if (gameChoice == "SCISSORS") {
-    gameResult = "YOU LOSE";
-  } else if (gameChoice == "ROCK") {
-    gameResult = "YOU WIN";
-  }
-  endGame();
-}
-
-function scissors() {
-  playerChoice = "SCISSORS";
-  gameChoice = gameArr[parseInt(Math.random() * 3)];
-  if (gameChoice == "SCISSORS") {
-    gameResult = "DRAW";
-  } else if (gameChoice == "ROCK") {
-    gameResult = "YOU LOSE";
-  } else if (gameChoice == "PAPER") {
-    gameResult = "YOU WIN";
-  }
-  endGame();
-}
-
-rockBtn.addEventListener("click", rock);
-paperBtn.addEventListener("click", paper);
-scissorsBtn.addEventListener("click", scissors);
+};
+// Events
+rockBtn.addEventListener("click", function () {
+  gameHandler(0);
+});
+paperBtn.addEventListener("click", function () {
+  gameHandler(1);
+});
+scissorsBtn.addEventListener("click", function () {
+  gameHandler(2);
+});
